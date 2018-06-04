ed286b1fe928ed6843d5b6a423b209acb2fb7fb3
Daniel Lillja <d_lillja@dell.com>
2018-06-04 18:17:57 -0500

    Start project

---
+++ DataStructures/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
