f08ef46b9a2b49d4dbaf1b63f94d0eb88da21009
Daniel Lillja <d_lillja@dell.com>
2018-06-13 16:38:16 -0500

    Implement initial linked list operations

---
+++ DataStructures/LinkedList/LinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.LinkedList
+{
+    internal class LinkedList : ICollection<int>
+    {
+        private int _count;
+        private LinkedListNode _head;
+        private LinkedListNode _tail;
+
+        public int Count => _count;
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        internal bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public void Add(int item)
+        {
+            if (IsEmpty())
+            {
+                _head = new LinkedListNode(item);
+                _tail = _head;
+            }
+            else if (_head == _tail)
+            {
+                _tail = new LinkedListNode(item);
+                _head.Next = _tail;
+            }
+
+            _count++;
+        }
+
+        class LinkedListNode
+        {
+            public int Key { get; set; }
+            public LinkedListNode Next { get; set; }
+            public LinkedListNode(int key) : this(key, null)
+            {
+            }
+            public LinkedListNode(int key, LinkedListNode next)
+            {
+                Key = key;
+                Next = next;
+            }
+        }
+
+        public int At(int index)
+        {
+            if (IsEmpty()) throw new ArgumentOutOfRangeException();
+
+            LinkedListNode cursor = _head;
+            var cursorIndex = 0;
+            while (cursorIndex != index)
+            {
+                cursor = cursor.Next;
+                cursorIndex++;
+            }
+
+            return cursor.Key;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(int[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataStructures/LinkedList/LinkedListTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DataStructures.LinkedList
+{
+    [TestClass]
+    public class LinkedListTests
+    {
+        private LinkedList _defaultList;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _defaultList = new LinkedList();
+        }
+
+        [TestMethod]
+        public void IsEmpty_empty_by_default()
+        {
+            Assert.IsTrue(_defaultList.IsEmpty());
+        }
+
+        [TestMethod]
+        public void Count_zero_by_default()
+        {
+            Assert.AreEqual(0, _defaultList.Count);
+        }
+
+        [TestMethod]
+        public void Add_increases_count_by_1()
+        {
+            _defaultList.Add(1);
+            Assert.AreEqual(1, _defaultList.Count);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void At_throws_by_default()
+        {
+            _defaultList.At(0);
+        }
+
+        [TestMethod]
+        public void At_returns_head_key_at_index_0()
+        {
+            _defaultList.Add(1);
+            Assert.AreEqual(1, _defaultList.At(0));
+        }
+
+        [TestMethod]
+        public void At_returns_correct_key_at_index_1()
+        {
+            _defaultList.Add(1);
+            _defaultList.Add(2);
+            Assert.AreEqual(2, _defaultList.At(1));
+        }
+    }
+}
